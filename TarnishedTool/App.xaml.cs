ae3f7db400260d2d26ff2eed98b342ae882a9def
William Bradford <wcb8ze@virginia.edu>
2026-06-23 14:13:23 -0400

    tweaked again to facilitate hyprland

--- TarnishedTool/App.xaml.cs
+++ TarnishedTool/App.xaml.cs
@@ -15,7 +15,7 @@ namespace TarnishedTool
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            AppContext.SetSwitch("Switch.System.Windows.Input.Stylus.DisableStylusAndTouchSupport", true);
+            System.AppContext.SetSwitch("Switch.System.Windows.Input.Stylus.DisableStylusAndTouchSupport", true);
             RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
 
             const string appName = "TarnishedTool";
